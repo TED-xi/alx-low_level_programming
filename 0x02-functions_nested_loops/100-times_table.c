a037b928ee1d3385b5e8eaa3fe3602c99f3c77c0
TED-xi <paul.moses27@gmail.com>
2022-11-05 18:24:16 -0700

    warm

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,21 +27,26 @@ void print_times_table(int n)
 				}
 				else if (c < 10)
 				{
-					_putchar(' '); _putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar('0' + f);
 				}
 				else if (c < 100)
 				{
 					_putchar(' ');
-					_putchar('0' + e); _putchar('0' + f);
+					_putchar('0' + e);
+					_putchar('0' + f);
 				}
 				else
 				{
-					_putchar('0' + d); _putchar('0' + e); _putchar('0' + f);
+					_putchar('0' + d);
+					_putchar('0' + e);
+					_putchar('0' + f);
 				}
 				if (b < n)
 				{
-					_putchar(','); _putchar(' ');
+					_putchar(',');
+					_putchar(' ');
 				}
 				else
 					_putchar('\n');
